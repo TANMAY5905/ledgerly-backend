fe79a214bdc7b63946b210422faf5ad14aee09ea
Tyrano-jpg <tanmay.kdc@gmail.com>
2026-03-26 12:57:36 +0530

    changes in cors file again

--- src/main/java/com/expensetracker/expense_tracker/config/CorsConfig.java
+++ src/main/java/com/expensetracker/expense_tracker/config/CorsConfig.java
@@ -16,11 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "http://127.0.0.1:5173",
-                "https://ledgerly-front.vercel.app"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
